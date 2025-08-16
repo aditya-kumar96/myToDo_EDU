19a770ddf1a564b5ab9506d2cd23aaca2f347620
Aditya Kumar <aadikori9085@gmail.com>
2025-08-16 16:28:14 +0530

    Integrating Convex with React Native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,16 @@
+import { api } from "@/convex/_generated/api";
 import useColorScheme from "@/hooks/useColorScheme";
+import { useMutation, useQuery } from "convex/react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const {toggleDarkMode} = useColorScheme()
+  const getTodo = useQuery(api.todos.getTodos);
+    console.log(getTodo);
+  const addTodo = useMutation(api.todos.addTodo);
+  const clearAllTodo = useMutation(api.todos.clearAllTodos)
+
+    
   return (
     <View
       style={style.container}
@@ -10,6 +18,15 @@ export default function Index() {
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle Mode</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={()=>addTodo({text:"Adding first Todo "})}>
+        <Text>Add First Todo</Text>
+      </TouchableOpacity>
+
+
+      <TouchableOpacity onPress={()=>clearAllTodo()}>
+        <Text>Clear All</Text>
       </TouchableOpacity>
     </View>
   );
@@ -19,6 +36,6 @@ const style = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor:'red'
+    
   }
 })
